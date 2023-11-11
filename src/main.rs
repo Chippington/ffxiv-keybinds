5ef997e51b5beefb721a7afbb8e91491908439c5
Gerrit Jamerson <gjamerson@gmail.com>
2023-11-10 17:29:20 -0800

    Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,59 @@
-use inputbot::{
-    KeySequence,
-    KeybdKey::{self, *},
-    MouseButton::{self, *},
-};
+use std::sync::{Arc, Mutex};
+
+use inputbot::*;
+
+struct Bindings {
+    to_bind: Vec<KeybdKey>,
+    next: usize,
+}
+
+impl Bindings {
+    fn new() -> Self {
+        let to_bind = vec![
+            KeybdKey::LControlKey,
+            KeybdKey::LAltKey,
+            KeybdKey::LShiftKey,
+        ];
+
+        println!("Press moust button to bind them:");
+        println!("{}: ", to_bind[0].to_string());
+        Bindings { to_bind, next: 0 }
+    }
+
+    fn update(&mut self, key: MouseButton) {
+        if self.next == self.to_bind.len() {
+            return;
+        }
+
+        let key_to_bind = self.to_bind[self.next];
+        println!("{} binded to {}", key_to_bind.to_string(), key.to_string());
+        key.bind(move || {
+            key_to_bind.press();
+            loop {
+                if key.is_pressed() == false {
+                    break;
+                }
+            }
+            key_to_bind.release();
+        });
+
+        self.next = self.next + 1;
+
+        if self.next < self.to_bind.len() {
+            println!("{}: ", self.to_bind[self.next].to_string());
+        } else {
+            println!("Finished configuring bindings");
+        }
+    }
+}
 
 fn main() {
-    MouseButton::bind_all(|i| {
-        println!("Pressed: {}", i.to_string());
-        loop {
-            if i.is_pressed() == false {
-                break;
-            }
-        }
-        println!("Released: {}", i.to_string());
+    let bindings = Bindings::new();
+    let bindings = Arc::new(Mutex::new(bindings));
+
+    MouseButton::bind_all(move |i| {
+        bindings.lock().unwrap().update(i);
     });
 
-    // println!("Hello, world!");
-    // MouseButton::X1Button.bind(|| {
-    //     println!("A");
-    //     loop {
-    //         if MouseButton::X1Button.is_pressed() == false {
-    //             break;
-    //         }
-    //     }
-    //     println!("B");
-    // });
-
-    // MouseButton::X2Button.bind(|| {
-    //     println!("C");
-    //     loop {
-    //         if MouseButton::X2Button.is_pressed() == false {
-    //             break;
-    //         }
-    //     }
-    //     println!("D");
-    // });
-
-    // Call this to start listening for bound inputs.
     inputbot::handle_input_events();
 }
